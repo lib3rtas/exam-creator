9733af94ba69f9280fd50f315e8c1144f15ed0b7
Florian Hammer <florian.hammer@students.fhnw.ch>
2020-09-25 19:29:34 +0200

    remodeled the generator to reader and generator

--- Generator.pl
+++ Generator.pl
@@ -10,9 +10,8 @@ use experimental 'switch';
 
 # load own class definitions
 use lib '.';
-use States;
 use Models;
-use Constants;
+use Reader;
 
 # Process arguments
 my $number_arguments = $#ARGV + 1;
@@ -20,103 +19,12 @@ unless($number_arguments == 1){
     die qq{Invalid number of arguments.};
 }
 
-# open master file
-my $master_file = $ARGV[0];
-open(FILE, '<', $master_file) or die $!; #TODO better debug information for user
+my $reader = Reader->new();
+my ($header, @questions) = $reader->read_exam($ARGV[0]);
 
-# initialize objects for line processing
-my $header = Header->new(
-    content => ""
-);
-my @questions = ();
-my $current_question = Question->new(
-    answers => [],
-    answers_value => [], 
-    question => ""
-);
-
-# process the master file linewise
-while(my $line = readline(FILE)){
-    state $current_state = Header_State->new();
-
-    given ($line) {
-
-        when ($Constants::question_regex) { 
-            $current_state->handle_question_line(
-                \$current_state,
-                \$line,
-                \$current_question,
-                \@questions,
-                $header
-            );
-        }
-
-        when ($Constants::unmarked_answer_regex) { 
-            $current_state->handle_unmarked_answer_line(
-                \$current_state,
-                \$line,
-                \$current_question,
-                \@questions,
-                $header
-            );
-        }
-
-        when ($Constants::marked_answer_regex) { 
-            $current_state->handle_marked_answer_line(
-                \$current_state,
-                \$line,
-                \$current_question,
-                \@questions,
-                $header
-            );
-        }
-
-        when ($Constants::seperator_regex) { 
-            $current_state->handle_seperator_line(
-                \$current_state,
-                \$line,
-                \$current_question,
-                \@questions,
-                $header
-            );
-        }
-
-        when ($Constants::text_regex) { 
-            $current_state->handle_text_line(
-                \$current_state,
-                \$line,
-                \$current_question,
-                \@questions,
-                $header
-            );
-        }
-
-        when ($Constants::empty_line_regex) { 
-            $current_state->handle_empty_line(
-                \$current_state,
-                \$line,
-                \$current_question,
-                \@questions,
-                $header
-            );
-        }
-        
-        default { 
-            $current_state->handle_unknown_line(
-                \$current_state,
-                \$line,
-                \$current_question,
-                \@questions,
-                $header
-            );
-        }
-    }
-}
-close(FILE);
-
-# generate an exam file
+#generate an exam file
 my $EXAM_FILE;
-open($EXAM_FILE, '>', create_exam_title()) or die $!;
+open($EXAM_FILE, '>', create_exam_title($ARGV[0])) or die $!;
 
 my $exam_string = "";
 $exam_string .= $header->content;
@@ -131,10 +39,10 @@ foreach my $question (@questions){
 print($EXAM_FILE $exam_string);
 close($EXAM_FILE);
 
-# helper methods
-sub create_exam_title(){
+# helper method
+sub create_exam_title($file_name) {
     my ($second,$minute,$hour,$day,$month,$year) = localtime();
     # create timestamp in format YYYYMMDD-hhmmss-
     my $timestamp = sprintf("%.4d%.2d%.2d-%.2d%.2d%.2d-", $year+1900, $month+1, $day, $hour, $minute, $second);
-    return "$timestamp$master_file";
+    return "$timestamp$file_name";
 }
